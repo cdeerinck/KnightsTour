9a1d778c2235584ee68a80b87901ae150502caaf
Chuck Deerinck <cjd9@me.com>
2020-02-28 14:22:40 -0800

    Initial Commit

---
+++ KnightsTour1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  KnightsTour1
+//
+//  Created by Chuck Deerinck on 2/28/20.
+//  Copyright © 2020 Chuck Deerinck. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
